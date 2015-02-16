ad500f41b3c8dd22842dd2904d3b569c360bf627
Henrique Lobo Weissmann <loboweissmann@gmail.com>
2015-02-16 16:00:11 -0200

    Enviando para outro PC

--- grails-app/controllers/concot/FornecedorController.groovy
+++ grails-app/controllers/concot/FornecedorController.groovy
@@ -8,6 +8,8 @@ class FornecedorController {
 		[fornecedores:Fornecedor.list(), mensagem:new EnvioEmail()]
 	}
 
+	def comunicacaoService
+
 	def enviarMensagem(EnvioEmail envio) {
 		envio.validate()
 		if (envio.hasErrors()) {
@@ -20,6 +22,9 @@ class FornecedorController {
 						   fornecedores:Fornecedor.list()])
 		} else {
 			flash.message = "Mensagem enviada com sucesso"
+			comunicacaoService.enviarMensagem(envio.fornecedor,
+											  envio.email,
+											  envio.mensagem)
 			render(view:'comunicacao')
 		}
 	}
---
+++ grails-app/services/concot/ComunicacaoService.groovy
@@ -0,0 +1,13 @@
+package concot
+
+import grails.transaction.Transactional
+
+@Transactional
+class ComunicacaoService {
+
+    def enviarMensagem(Fornecedor fornecedor, 
+					   String email,
+					   String mensagem) {
+		// Executa a tarefa de envio de e-mail
+	}
+}
